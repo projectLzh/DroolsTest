bcb34248d67bb0de6501ec523d24b667d51791ce
来家二少 <kangzuguan@qq.com>
2019-06-26 16:47:26 +0800

    Update RuleName.java
    修改问题

--- src/main/java/comTwo/ruleName/RuleName.java
+++ src/main/java/comTwo/ruleName/RuleName.java
@@ -20,7 +20,7 @@ public class RuleName {
     public void testRuleName() {
         KieServices kss = KieServices.Factory.get();
         KieContainer kc = kss.getKieClasspathContainer();
-        KieSession ks = kc.newKieSession("isString");
+        KieSession ks = kc.newKieSession("isRuleName");
         int count = ks.fireAllRules();
         System.out.println("总执行了" + count + "条规则");
         ks.dispose();
@@ -30,7 +30,7 @@ public class RuleName {
     public void testRuleName2() {
         KieServices kss = KieServices.Factory.get();
         KieContainer kc = kss.getKieClasspathContainer();
-        KieSession ks = kc.newKieSession("isString");
+        KieSession ks = kc.newKieSession("isRuleName");
         int count = ks.fireAllRules(new RuleNameEqualsAgendaFilter("指定规则名"));
         System.out.println("总执行了" + count + "条规则");
         ks.dispose();
@@ -40,7 +40,7 @@ public class RuleName {
     public void testRuleName3() {
         KieServices kss = KieServices.Factory.get();
         KieContainer kc = kss.getKieClasspathContainer();
-        KieSession ks = kc.newKieSession("isString");
+        KieSession ks = kc.newKieSession("isRuleName");
         int count = ks.fireAllRules(new RuleNameEndsWithAgendaFilter("三"));
         System.out.println("总执行了" + count + "条规则");
         ks.dispose();
@@ -50,7 +50,7 @@ public class RuleName {
     public void testRuleName4() {
         KieServices kss = KieServices.Factory.get();
         KieContainer kc = kss.getKieClasspathContainer();
-        KieSession ks = kc.newKieSession("isString");
+        KieSession ks = kc.newKieSession("isRuleName");
         int count = ks.fireAllRules(new RuleNameStartsWithAgendaFilter("指定"));
         System.out.println("总执行了" + count + "条规则");
         ks.dispose();
@@ -61,7 +61,7 @@ public class RuleName {
     public void testRuleName5() {
         KieServices kss = KieServices.Factory.get();
         KieContainer kc = kss.getKieClasspathContainer();
-        KieSession ks = kc.newKieSession("isString");
+        KieSession ks = kc.newKieSession("isRuleName");
         int count = ks.fireAllRules(new RuleNameMatchesAgendaFilter("\\w{0,10}"));
         System.out.println("总执行了" + count + "条规则");
         ks.dispose();
@@ -70,7 +70,7 @@ public class RuleName {
     public void testRuleName6() {
         KieServices kss = KieServices.Factory.get();
         KieContainer kc = kss.getKieClasspathContainer();
-        KieSession ks = kc.newKieSession("isString");
+        KieSession ks = kc.newKieSession("isRuleName");
         Set set=new HashSet();
         set.add("指定规则名");
         set.add("指定规则名四");
